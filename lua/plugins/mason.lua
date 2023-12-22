3238e45f23b38047fe94f262821f79d7e6d8ebc8
Thomas Becker <thomas.becker00@gmail.com>
2023-12-22 17:33:11 +0100

    extract mason to it's own file

---
+++ lua/plugins/mason.lua
@@ -0,0 +1,25 @@
+return {
+  {
+    "williamboman/mason.nvim",
+    opts = {
+      ensure_installed = {
+        "checkstyle",
+        "docker-compose-language-service",
+        "dockerfile-language-server",
+        "eslint-lsp",
+        "flake8",
+        "json-lsp",
+        "markdownlint",
+        "prettier",
+        "shellcheck",
+        "shfmt",
+        "sonarlint-language-server",
+        "stylua",
+        "tailwindcss-language-server",
+        "typescript-language-server",
+        "yaml-language-server",
+        "yamllint",
+      },
+    },
+  },
+}
