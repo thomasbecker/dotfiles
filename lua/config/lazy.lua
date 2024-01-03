2f74b47cfb6419016c30bae7f68fbbe50fa53e77
Thomas Becker <thomas.becker00@gmail.com>
2024-01-03 12:48:26 +0100

    add typescript and json extras

--- lua/config/lazy.lua
+++ lua/config/lazy.lua
@@ -13,8 +13,8 @@ require("lazy").setup({
       colorscheme = "catppuccin",
     } },
     -- import any extras modules here
-    -- { import = "lazyvim.plugins.extras.lang.typescript" },
-    -- { import = "lazyvim.plugins.extras.lang.json" },
+    { import = "lazyvim.plugins.extras.lang.typescript" },
+    { import = "lazyvim.plugins.extras.lang.json" },
     -- { import = "lazyvim.plugins.extras.ui.mini-animate" },
     -- import/override with your plugins
     { import = "plugins" },
