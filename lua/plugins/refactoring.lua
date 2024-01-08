0e393042f70ddd3940252e78af41802888956380
Thomas Becker <thomas.becker00@gmail.com>
2024-01-08 12:09:34 +0100

    add ThePrimeagen/refactoring

---
+++ lua/plugins/refactoring.lua
@@ -0,0 +1,37 @@
+return {
+  {
+    "ThePrimeagen/refactoring.nvim",
+    dependencies = {
+      "nvim-lua/plenary.nvim",
+      "nvim-treesitter/nvim-treesitter",
+      "nvim-treesitter/playground",
+    },
+    cmd = { "Refactoring" },
+    init = function()
+      require("refactoring").setup({
+        prompt_func_return_type = {
+          java = true,
+        },
+        prompt_func_param_type = {
+          java = true,
+        },
+      })
+    end,
+    keys = {
+      { mode = { "x" }, "<leader>re", "<cmd>Refactor extract<cr>", desc = "Refactor extract" },
+      { mode = { "x" }, "<leader>rf", "<cmd>Refactor extract_to_file<cr>", desc = "Refactor extract to file" },
+      { mode = { "x" }, "<leader>rv", "<cmd>Refactor extract_var<cr>", desc = "Refactor extract variable" },
+
+      { mode = { "n", "x" }, "<leader>ri", "<cmd>Refactor extract_var<cr>", desc = "Refactor extract variable" },
+      { mode = { "n" }, "<leader>rI", "<cmd>Refactor inline_func<cr>", desc = "Refactor inline function" },
+
+      { mode = { "n" }, "<leader>rb", "<cmd>Refactor extract_block<cr>", desc = "Refactor extract block" },
+      {
+        mode = { "n" },
+        "<leader>rbf",
+        "<cmd>Refactor extract_block_to_file<cr>",
+        desc = "Refactor extract block to file",
+      },
+    },
+  },
+}
