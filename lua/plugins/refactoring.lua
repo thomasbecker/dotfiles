dbc9a0af7ee66a70c061e497af7df3ed171e64a5
Thomas Becker <thomas.becker00@gmail.com>
2024-01-13 21:54:49 +0100

    feat: add go to refactoring plugin

--- lua/plugins/refactoring.lua
+++ lua/plugins/refactoring.lua
@@ -11,10 +11,13 @@ return {
       require("refactoring").setup({
         prompt_func_return_type = {
           java = true,
+          go = true,
         },
         prompt_func_param_type = {
           java = true,
+          go = true,
         },
+        show_success_message = true,
       })
     end,
     keys = {
