8abc6b25be1d79bd3520ab670065f6a10a56b1f4
Thomas Becker <thomas.becker00@gmail.com>
2024-01-20 08:55:37 +0100

    feat: add some go plugin key mappings

--- lua/plugins/go.lua
+++ lua/plugins/go.lua
@@ -8,9 +8,19 @@ return {
     },
     config = function()
       require("go").setup()
+      require("go.format").gofmt()
+      require("go.format").goimport()
     end,
     event = { "CmdlineEnter" },
     ft = { "go", "gomod" },
     build = ':lua require("go.install").update_all_sync()', -- if you need to install/update all binaries
+    keys = {
+      { mode = { "n" }, "<leader>gb", "<cmd>GoBuild<cr>", desc = "Go build" },
+      { mode = { "n" }, "<leader>gr", "<cmd>GoRun<cr>", desc = "Go run" },
+      { mode = { "n" }, "<leader>gt", "<cmd>GoTest<cr>", desc = "Go test" },
+      { mode = { "n" }, "<leader>gc", "<cmd>GoCoverage<cr>", desc = "Go coverage toggle" },
+      { mode = { "n" }, "<leader>gl", "<cmd>GoLint<cr>", desc = "Go lint" },
+      { mode = { "n" }, "<leader>gd", "<cmd>GoDoc<cr>", desc = "Go doc" },
+    },
   },
 }
