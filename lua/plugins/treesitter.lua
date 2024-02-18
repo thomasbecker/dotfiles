a7abf2807ef0ca1b3e67a90eda824208975d1ea3
Thomas Becker <thomas.becker00@gmail.com>
2024-02-18 15:21:18 +0100

    fix: treesitter textobject mappings

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -28,43 +28,43 @@ return {
         "vimdoc",
         "yaml",
       },
-    },
-    textobjects = {
-      move = {
-        enable = true,
-        goto_next_start = {
-          ["]f"] = "@function.outer",
-          ["]c"] = "@class.outer",
+      textobjects = {
+        move = {
+          enable = true,
+          goto_next_start = {
+            ["]f"] = "@function.outer",
+            ["]c"] = "@class.outer",
+          },
+          goto_next_end = {
+            ["]F"] = "@function.outer",
+            ["]C"] = "@class.outer",
+          },
+          goto_previous_start = {
+            ["[f"] = "@function.outer",
+            ["[c"] = "@class.outer",
+          },
+          goto_previous_end = {
+            ["[F"] = "@function.outer",
+            ["[C"] = "@class.outer",
+          },
+          -- Below will go to either the start or the end, whichever is closer.
+          -- Use if you want more granular movements
+          -- Make it even more gradual by adding multiple queries and regex.
+          goto_next = {
+            ["]o"] = "@conditional.outer",
+          },
+          goto_previous = {
+            ["[o"] = "@conditional.outer",
+          },
         },
-        goto_next_end = {
-          ["]F"] = "@function.outer",
-          ["]C"] = "@class.outer",
-        },
-        goto_previous_start = {
-          ["[f"] = "@function.outer",
-          ["[c"] = "@class.outer",
-        },
-        goto_previous_end = {
-          ["[F"] = "@function.outer",
-          ["[C"] = "@class.outer",
-        },
-        -- Below will go to either the start or the end, whichever is closer.
-        -- Use if you want more granular movements
-        -- Make it even more gradual by adding multiple queries and regex.
-        goto_next = {
-          ["]o"] = "@conditional.outer",
-        },
-        goto_previous = {
-          ["[o"] = "@conditional.outer",
-        },
-      },
-      swap = {
-        enable = true,
-        swap_next = {
-          ["<leader>i"] = "@parameter.inner",
-        },
-        swap_previous = {
-          ["<leader>I"] = "@parameter.inner",
+        swap = {
+          enable = true,
+          swap_next = {
+            ["<leader>i"] = "@parameter.inner",
+          },
+          swap_previous = {
+            ["<leader>I"] = "@parameter.inner",
+          },
         },
       },
     },
