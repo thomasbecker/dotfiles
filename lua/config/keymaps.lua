1bc40f621b77769331a6ad5f4548dc6c9454964f
Thomas Becker <thomas.becker00@gmail.com>
2024-02-19 09:50:49 +0100

    feat: add key mappings for inserting new lines
    And also set custom keymaps the lazy.vim way

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -1,6 +1,11 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
+local Util = require("lazyvim.util")
+local map = Util.safe_keymap_set
 
-vim.keymap.set("n", "<C-d>", "<C-d>zz") -- move half page down and center
-vim.keymap.set("n", "<C-u>", "<C-u>zz") -- move half page up and center
+map("n", "<C-d>", "<C-d>zz", { desc = "Move half page down and center cursor" })
+map("n", "<C-u>", "<C-u>zz", { desc = "Move half page up and center cursor" })
+
+map("n", "o", 'o<ESC>0"_D', { desc = "Insert new line below cursor" })
+map("n", "O", 'O<ESC>0"_D', { desc = "Insert new line above cursor" })
