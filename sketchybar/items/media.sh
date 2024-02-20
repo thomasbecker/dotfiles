95bd5d60dc75503d8212890c7825df4707639837
Thomas Becker <thomas.becker00@gmail.com>
2024-02-20 13:17:12 +0100

    fix: some sketchybar fixes

--- sketchybar/items/media.sh
+++ sketchybar/items/media.sh
@@ -1,10 +1,9 @@
 media=(
 	icon.background.drawing=on
 	icon.background.image=media.artwork
-	icon.background.image.corner_radius=9
-	label.padding_left=15
+	icon.background.image.corner_radius=6
 	script="$PLUGIN_DIR/media.sh"
-	label.max_chars=c5
+	label.max_chars=35
 	scroll_texts=on
 	updates=on
 )
