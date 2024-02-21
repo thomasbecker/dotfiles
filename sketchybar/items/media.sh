dde11cce9a502bff484f9d89c83daf3b57fb3b67
Thomas Becker <thomas.becker00@gmail.com>
2024-02-21 07:20:31 +0100

    feat: reduce size of media artwork image in sketchybar

--- sketchybar/items/media.sh
+++ sketchybar/items/media.sh
@@ -2,6 +2,7 @@ media=(
 	icon.background.drawing=on
 	icon.background.image=media.artwork
 	icon.background.image.corner_radius=6
+	icon.background.image.scale=0.8
 	script="$PLUGIN_DIR/media.sh"
 	label.max_chars=35
 	scroll_texts=on
