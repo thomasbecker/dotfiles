1a44f4efa3ec03078c600419c844978f3d16b8f9
Thomas Becker <thomas.becker00@gmail.com>
2024-02-23 16:54:35 +0100

    refactor: configure ip_address via an array

--- sketchybar/items/ip_address.sh
+++ sketchybar/items/ip_address.sh
@@ -1,11 +1,15 @@
+ip_address=(
+	background.border_color="$BACKGROUND_2"
+	update_freq=30
+	padding_left=2
+	padding_right=8
+	icon.highlight=on
+	icon.padding_left=5
+	label.highlight=on
+	label.padding_right=8
+	script="$PLUGIN_DIR/ip_address.sh"
+)
+
 sketchybar --add item ip_address right \
-	--set ip_address script="$PLUGIN_DIR/ip_address.sh" \
-	background.border_color="$BACKGROUND_2" \
-	update_freq=30 \
-	padding_left=2 \
-	padding_right=8 \
-	icon.highlight=on \
-	icon.padding_left=5 \
-	label.highlight=on \
-	label.padding_right=8 \
+	--set ip_address "${ip_address[@]}" \
 	--subscribe ip_address wifi_change
