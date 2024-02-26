196dd36b2130c789582e88750cb8cd51fa6c5486
Thomas Becker <thomas.becker00@gmail.com>
2024-02-26 09:25:50 +0100

    feat: change vault folder to share with mobile devices
    and some other obsidian configs

--- lua/plugins/obsidian.lua
+++ lua/plugins/obsidian.lua
@@ -12,14 +12,31 @@ return {
   opts = {
     workspaces = {
       {
-        name = "personal",
-        path = "~/Library/Mobile Documents/com~apple~CloudDocs/Documents/obsidian/personal",
+        name = "Work",
+        path = "/Users/tbecker/Library/Mobile Documents/iCloud~md~obsidian/Documents/Work",
       },
       {
-        name = "work",
-        path = "~/Library/Mobile Documents/com~apple~CloudDocs/Documents/obsidian/work",
+        name = "Personal",
+        path = "/Users/tbecker/Library/Mobile Documents/iCloud~md~obsidian/Documents/Personal",
       },
     },
+    -- Optional, customize how names/IDs for new notes are created.
+    note_id_func = function(title)
+      -- Create note IDs in a Zettelkasten format with a timestamp and a suffix.
+      -- In this case a note with the title 'My new note' will be given an ID that looks
+      -- like '1657296016-my-new-note', and therefore the file name '1657296016-my-new-note.md'
+      local suffix = ""
+      if title ~= nil then
+        -- If title is given, transform it into valid file name.
+        suffix = title:gsub(" ", "-"):gsub("[^A-Za-z0-9-]", ""):lower()
+      else
+        -- If title is nil, just add 4 random uppercase letters to the suffix.
+        for _ = 1, 4 do
+          suffix = suffix .. string.char(math.random(65, 90))
+        end
+      end
+      return tostring(os.time()) .. "-" .. suffix
+    end,
   },
   keys = {
     { mode = { "n" }, "<leader>on", "<cmd>ObsidianNew<cr>", desc = "Obsidian new" },
