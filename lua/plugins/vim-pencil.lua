6b9c30747b7bc45331305e140ac35d730bb2ce11
Thomas Becker <thomas.becker00@gmail.com>
2024-02-27 10:41:32 +0100

    feat: add keymap to toggle pencil format
    As pencil breaks obsidian md files I need a way to quickly disable it

--- lua/plugins/vim-pencil.lua
+++ lua/plugins/vim-pencil.lua
@@ -1,5 +1,10 @@
 return {
   {
     "preservim/vim-pencil",
+    lazy = true,
+    -- enabled = false,
+    keys = {
+      { mode = { "n" }, "<leader>bf", "<cmd>PFormatToggle<cr>", desc = "Toggle pencil format" },
+    },
   },
 }
