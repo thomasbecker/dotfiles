a539ed91f37b874b9e0be14054e1c08e32c7ecd0
Thomas Becker <thomas.becker00@gmail.com>
2024-03-01 06:59:14 +0100

    chore: remove example.lua

--- init.lua
+++ init.lua
@@ -1,2 +1,14 @@
+-- try neovide
+if vim.g.neovide then
+  vim.o.guifont = "JetBrainsMono Nerd Font Mono:h14" -- text below applies for VimScript
+
+  vim.g.neovide_padding_top = 0
+  vim.g.neovide_padding_bottom = 0
+  vim.g.neovide_padding_right = 0
+  vim.g.neovide_padding_left = 0
+
+  vim.g.neovide_transparency = 0.9
+end
+
 -- bootstrap lazy.nvim, LazyVim and your plugins
 require("config.lazy")
