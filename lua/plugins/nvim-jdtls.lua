9622c7e59789c2a1170d8ac0172c98e25a3dc64d
Thomas Becker <thomas.becker00@gmail.com>
2024-03-03 13:15:32 +0100

    feat: add lombok java agent for lombok annotation processing and class generation

---
+++ lua/plugins/nvim-jdtls.lua
@@ -0,0 +1,12 @@
+local java_filetypes = { "java" }
+return {
+  {
+    "mfussenegger/nvim-jdtls",
+    opts = {
+      cmd = {
+        vim.fn.exepath("jdtls"),
+        "--jvm-arg=" .. string.format("-javaagent:%s", vim.fn.expand("$MASON/share/jdtls/lombok.jar")),
+      },
+    },
+  },
+}
