f5653f843fd2d6a0d44b1ca4c0ce5a572c94a98c
Thomas Becker <thomas.becker00@gmail.com>
2024-03-16 12:51:45 +0100

    fix: make toggle-gaps script work with the latest yabai release

--- skhd/toggle-gaps.sh
+++ skhd/toggle-gaps.sh
@@ -3,10 +3,12 @@ borderFile=/tmp/borders
 if [[ -f /tmp/borders ]]; then
 	borders width=0
 	yabai -m config external_bar main:0:0
+	yabai -m space --padding abs:2:2:2:2 --gap abs:3
 	rm $borderFile
 else
 	borders width=5
 	yabai -m config external_bar main:27:0
+	yabai -m space --padding abs:10:10:10:10 --gap abs:6
 	touch $borderFile
 fi
 
