402aa3b724b7304371b137a96c91a18abbaadefc
Thomas Becker <thomas.becker00@gmail.com>
2024-03-18 10:57:39 +0100

    feat: apply some small color and border adjustments

--- sketchybar/colors.sh
+++ sketchybar/colors.sh
@@ -28,14 +28,14 @@ export GREY=0xff939ab7
 export TRANSPARENT=0x00000000
 export BG0=0xff1e1e2e
 export BG1=0x603c3e4f
-export BG2=0x60494d64
+export BG2=0xff9b9b9b
 
 # General bar colors
 export BAR_COLOR=$BG0
 export BAR_BORDER_COLOR=$BG2
 export BACKGROUND_1=$BG1
 export BACKGROUND_2=$BG2
-export ICON_COLOR=$WHITE # Color of all icons
+export ICON_COLOR=$WHITE  # Color of all icons
 export LABEL_COLOR=$WHITE # Color of all labels
 export POPUP_BACKGROUND_COLOR=$BAR_COLOR
 export POPUP_BORDER_COLOR=$WHITE
