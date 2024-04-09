4d0e7552319abdec4714d2732a1ca918b72564a8
Thomas Becker <thomas.becker00@gmail.com>
2024-04-09 22:56:41 +0200

    feat: add key bindings for Copilot chat and lazy load it
    This commit introduces new key bindings for various Copilot
    chatfunctions. These include opening, toggling, explaining,
    fixing,optimizing, and committing staged changes in the Copilot chat.

--- lua/plugins/copilotChat.lua
+++ lua/plugins/copilotChat.lua
@@ -6,9 +6,18 @@ return {
       { "zbirenbaum/copilot.lua" }, -- or github/copilot.vim
       { "nvim-lua/plenary.nvim" }, -- for curl, log wrapper
     },
+    lazy = true,
     opts = {
       debug = false, -- Enable debugging
       -- See Configuration section for rest
+    },
+    keys = {
+      { "<leader>bco", "<cmd>CopilotChatOpen<cr>", desc = "Open Copilot chat" },
+      { "<leader>bct", "<cmd>CopilotChatOpen<cr>", desc = "Toggle Copilot chat" },
+      { "<leader>bce", "<cmd>CopilotChatExplain<cr>", desc = "Copilot explain" },
+      { "<leader>bcf", "<cmd>CopilotChatFix<cr>", desc = "Copilot fix" },
+      { "<leader>bco", "<cmd>CopilotChatOptimize<cr>", desc = "Copilot optimize" },
+      { "<leader>bcc", "<cmd>CopilotChatCommitStaged<cr>", desc = "Copilot write commit message for staged changes" },
     },
     -- See Commands section for default commands if you want to lazy load on them
   },
