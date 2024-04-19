6f000c4ab04856f2329239dd234fdcd459850a8c
Thomas Becker <thomas.becker00@gmail.com>
2024-04-19 08:52:28 +0200

    feat: increase pencil textwidth from 72 to 80

--- lua/plugins/vim-pencil.lua
+++ lua/plugins/vim-pencil.lua
@@ -3,6 +3,9 @@ return {
     "preservim/vim-pencil",
     lazy = true,
     -- enabled = false,
+    config = function()
+      vim.g["pencil#textwidth"] = 80
+    end,
     keys = {
       { mode = { "n" }, "<leader>bf", "<cmd>PencilToggle<cr>", desc = "Toggle pencil format" },
       { mode = { "n" }, "<leader>bt", "<cmd>PencilHard<cr>", desc = "PencilHard" },
