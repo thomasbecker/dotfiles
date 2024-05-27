d1d8b4ae9386ebb3d88af48ce336a08d2aab95b4
Thomas Becker <thomas.becker00@gmail.com>
2024-05-27 21:25:03 +0200

    chore(oil): rename config and remove some mappings

--- lua/plugins/oil.lua
+++ lua/plugins/oil.lua
@@ -7,5 +7,11 @@ return {
     keys = {
       { mode = { "n" }, "<leader>oi", "<cmd>Oil<cr>", desc = "Open parent directory" },
     },
+    opts = {
+      keymaps = {
+        ["<C-v>"] = "actions.select_vsplit",
+        ["<C-s>"] = nil,
+      },
+    },
   },
 }
