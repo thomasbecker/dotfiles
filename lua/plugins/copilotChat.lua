9b814b7356e45243d173620c9e46f501fa0247da
Thomas Becker <thomas.becker00@gmail.com>
2024-06-06 16:09:13 +0200

    chore(copilot): switch mostly to lazyvims default setup and bindings

--- lua/plugins/copilotChat.lua
+++ lua/plugins/copilotChat.lua
@@ -1,38 +1,29 @@
 return {
   {
     "CopilotC-Nvim/CopilotChat.nvim",
-    branch = "canary",
-    dependencies = {
-      { "zbirenbaum/copilot.lua" }, -- or github/copilot.vim { "nvim-lua/plenary.nvim" }, -- for curl, log wrapper
-    },
     lazy = true,
     opts = {
       debug = false, -- Enable debugging
       mappings = {
         reset = {
-          normal = "C-r",
-          insert = "C-r",
+          normal = "C-x",
+          insert = "C-x",
         },
+      },
+      window = {
+        width = 0.7, -- fractional width of parent, or absolute width in columns when > 1
       },
     },
     keys = {
-      { "<leader>bco", "<cmd>CopilotChatOpen<cr>", desc = "Open Copilot chat" },
-      { "<leader>bct", "<cmd>CopilotChatOpen<cr>", desc = "Toggle Copilot chat" },
-      { mode = { "n", "v" }, "<leader>bce", "<cmd>CopilotChatExplain<cr>", desc = "lopilot explain" },
-      { mode = { "n", "v" }, "<leader>bcf", "<cmd>CopilotChatFix<cr>", desc = "Copilot fix" },
-      { mode = { "n", "v" }, "<leader>bco", "<cmd>CopilotChatOptimize<cr>", desc = "Copilot optimize" },
-      { "<leader>bcc", "<cmd>CopilotChatCommitStaged<cr>", desc = "Copilot write commit message for staged changes" },
+      { mode = { "n", "v" }, "<leader>ae", "<cmd>CopilotChatExplain<cr>", desc = "Copilot explain" },
+      { mode = { "n", "v" }, "<leader>af", "<cmd>CopilotChatFix<cr>", desc = "Copilot fix" },
+      { mode = { "n", "v" }, "<leader>ao", "<cmd>CopilotChatOptimize<cr>", desc = "Copilot optimize" },
       {
-        "<leader>bcq",
-        function()
-          local input = vim.fn.input("Quick Chat: ")
-          if input ~= "" then
-            require("CopilotChat").ask(input, { selection = require("CopilotChat.select").buffer })
-          end
-        end,
-        desc = "Copilot quick chat with current buffer",
+        mode = { "n", "v" },
+        "<leader>ac",
+        "<cmd>CopilotChatCommitStaged<cr>",
+        desc = "Copilot write commit message for staged changes",
       },
     },
-    -- See Commands section for default commands if you want to lazy load on them
   },
 }
