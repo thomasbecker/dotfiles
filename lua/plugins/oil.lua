507870cb025311ff3291e9c654dd019a98579aef
Thomas Becker <thomas.becker00@gmail.com>
2024-06-10 23:16:24 +0200

    fix(oil): remove manual setup and fix keymaps for splits

--- lua/plugins/oil.lua
+++ lua/plugins/oil.lua
@@ -1,17 +1,27 @@
 return {
-
   {
     "stevearc/oil.nvim",
     dependencies = { "nvim-tree/nvim-web-devicons" },
     lazy = true,
-    keys = {
-      { mode = { "n" }, "<leader>oi", "<cmd>Oil<cr>", desc = "Open parent directory" },
-    },
     opts = {
       keymaps = {
-        ["<C-v>"] = "actions.select_vsplit",
-        ["<C-s>"] = nil,
+        ["<S-v>"] = "actions.select_vsplit",
+        ["<S-h>"] = "actions.select_split",
+        ["<C-s>"] = false,
+        ["<C-h>"] = false,
       },
+      columns = {
+        "icon",
+        -- "permissions",
+        -- "size",
+        -- "mtime",
+      },
+      view_options = {
+        show_hidden = false,
+      },
+    },
+    keys = {
+      { mode = { "n" }, "<leader>oi", "<cmd>Oil<cr>", desc = "Open parent directory" },
     },
   },
 }
