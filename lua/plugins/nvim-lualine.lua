efee23f4a8ff4b5afa93bcf5f15e31bff3ff209e
Thomas Becker <thomas.becker00@gmail.com>
2024-06-13 07:17:50 +0200

    chore(lualine): create catppuccin color theme for lualine

--- lua/plugins/nvim-lualine.lua
+++ lua/plugins/nvim-lualine.lua
@@ -1,11 +1,43 @@
 local colors = require("../config/colors")
 
+local catppuccin = {
+  normal = {
+    a = { bg = colors.mauve, fg = colors.surface0, gui = "bold" },
+    b = { bg = colors.surface1, fg = colors.mauve },
+    c = { bg = colors.surface0, fg = colors.text },
+  },
+  insert = {
+    a = { bg = colors.sky, fg = colors.surface0, gui = "bold" },
+    b = { bg = colors.surface1, fg = colors.sky },
+    c = { bg = colors.surface0, fg = colors.text },
+  },
+  visual = {
+    a = { bg = colors.blue, fg = colors.surface0, gui = "bold" },
+    b = { bg = colors.surface1, fg = colors.blue },
+    c = { bg = colors.surface0, fg = colors.text },
+  },
+  replace = {
+    a = { bg = colors.red, fg = colors.surface0, gui = "bold" },
+    b = { bg = colors.surface1, fg = colors.red },
+    c = { bg = colors.surface0, fg = colors.text },
+  },
+  command = {
+    a = { bg = colors.maroon, fg = colors.surface0, gui = "bold" },
+    b = { bg = colors.surface1, fg = colors.maroon },
+    c = { bg = colors.surface0, fg = colors.text },
+  },
+  inactive = {
+    a = { bg = colors.teal, fg = colors.surface0, gui = "bold" },
+    b = { bg = colors.surface1, fg = colors.teal },
+    c = { bg = colors.surface0, fg = colors.text },
+  },
+}
 return {
   {
     "nvim-lualine/lualine.nvim",
     opts = {
       options = {
-        theme = "palenight",
+        theme = catppuccin,
       },
       sections = {
         lualine_x = {
