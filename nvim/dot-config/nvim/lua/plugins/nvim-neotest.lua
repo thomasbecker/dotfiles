fc0c17247cba7ade046fe5ac1481a1d08a91af55
Thomas Becker <thomas.becker00@gmail.com>
2024-07-02 08:21:18 +0200

    feat: add neotest-jest

--- nvim/dot-config/nvim/lua/plugins/nvim-neotest.lua
+++ nvim/dot-config/nvim/lua/plugins/nvim-neotest.lua
@@ -1,6 +1,21 @@
 return {
   {
     "nvim-neotest/neotest",
+    dependencies = {
+      "nvim-neotest/neotest-jest",
+    },
+    opts = {
+      adapters = {
+        ["neotest-jest"] = {
+          jestCommand = "npm test --",
+          jestConfigFile = "jest.config.ts",
+          env = { CI = true },
+          cwd = function()
+            return vim.fn.getcwd()
+          end,
+        },
+      },
+    },
     keys = {
       {
         "<leader>tc",
