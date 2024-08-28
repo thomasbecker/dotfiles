ada17106fa4789f7d0b071b8aaa5ea9159c03bfc
Thomas Becker <thomas.becker00@gmail.com>
2024-08-28 09:00:56 +0200

    feat(keymaps): try to add a keymap to replace camel with snake case
    Doesn't work yet, no time to finish it atm.

--- nvim/dot-config/nvim/lua/config/keymaps.lua
+++ nvim/dot-config/nvim/lua/config/keymaps.lua
@@ -20,3 +20,6 @@ map("i", "<C-T-k>", "<Esc>:m .-2<CR>==gi", { desc = "Move line up" })
 
 -- Add word to spell list and jump to next misspelled word
 map("n", "zg", "zg]s", { desc = "Add word to spell list and jump to next misspelled word" })
+
+-- work in progsess
+-- map("n", "<leader>rs", "<cmd>g;<c-r><c-w>; s;[a-z]@<=[A-Z];_l&;g<cr>", { desc = "Replace camel case with snake case" })
