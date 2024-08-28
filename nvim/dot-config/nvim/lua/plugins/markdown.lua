d15e27052faa77be77566a96bba187c9ea8845ba
Thomas Becker <thomas.becker00@gmail.com>
2024-08-28 09:02:31 +0200

    chore(markdown): customize some settings

---
+++ nvim/dot-config/nvim/lua/plugins/markdown.lua
@@ -0,0 +1,17 @@
+return {
+  {
+    "MeanderingProgrammer/markdown.nvim",
+    opts = {
+      file_types = { "markdown", "norg", "rmd", "org" },
+      code = {
+        sign = true,
+        width = "block",
+        right_pad = 1,
+      },
+      heading = {
+        sign = true,
+        icons = { "󰲡 ", "󰲣 ", "󰲥 ", "󰲧 ", "󰲩 ", "󰲫 " },
+      },
+    },
+  },
+}
