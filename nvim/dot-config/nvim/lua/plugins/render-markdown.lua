00ba5a8dd1d636160a35c57b341231aaa14d7e41
Thomas Becker <thomas.becker00@gmail.com>
2024-11-11 14:29:22 +0100

    chore(render-markdown): uncomment right_pad

--- nvim/dot-config/nvim/lua/plugins/render-markdown.lua
+++ nvim/dot-config/nvim/lua/plugins/render-markdown.lua
@@ -1,23 +1,23 @@
 return {
-    {
-        "MeanderingProgrammer/render-markdown.nvim",
-        opts = {
-            code = {
-                sign = true,
-                width = "block",
-                -- right_pad = 1,
-            },
-            heading = {
-                sign = true,
-                icons = { "󰲡 ", "󰲣 ", "󰲥 ", "󰲧 ", "󰲩 ", "󰲫 " },
-            },
-            bullet = {
-                enabled = true,
-                icons = { "●", "○", "◆", "◇" },
-                left_pad = 0,
-                right_pad = 0,
-                highlight = "RenderMarkdownBullet",
-            },
-        },
+  {
+    "MeanderingProgrammer/render-markdown.nvim",
+    opts = {
+      code = {
+        sign = true,
+        width = "block",
+        right_pad = 1,
+      },
+      heading = {
+        sign = true,
+        icons = { "󰲡 ", "󰲣 ", "󰲥 ", "󰲧 ", "󰲩 ", "󰲫 " },
+      },
+      bullet = {
+        enabled = true,
+        icons = { "●", "○", "◆", "◇" },
+        left_pad = 0,
+        right_pad = 0,
+        highlight = "RenderMarkdownBullet",
+      },
     },
+  },
 }
