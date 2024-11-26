871e22212010a9e870f65b51a5feb94e7f827a94
Thomas Becker <thomas.becker00@gmail.com>
2024-11-26 17:12:36 +0100

    chore(nvim): change layout of telescope-undo

--- nvim/dot-config/nvim/lua/plugins/telescope-undo.lua
+++ nvim/dot-config/nvim/lua/plugins/telescope-undo.lua
@@ -15,15 +15,15 @@ return {
       },
     },
     opts = {
-      -- extensions = {
-      --   undo = {
-      --     side_by_side = true,
-      --     layout_strategy = "vertical",
-      --     layout_config = {
-      --       preview_height = 0.8,
-      --     },
-      --   },
-      -- },
+      extensions = {
+        undo = {
+          side_by_side = true,
+          layout_strategy = "vertical",
+          layout_config = {
+            preview_height = 0.7,
+          },
+        },
+      },
     },
     config = function(_, opts)
       -- Calling telescope's setup from multiple specs does not hurt, it will happily merge the
