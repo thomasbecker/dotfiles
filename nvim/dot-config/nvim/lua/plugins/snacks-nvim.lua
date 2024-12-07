e2f0737c064a97fda46b3280d474f56f21b75631
Thomas Becker <thomas.becker00@gmail.com>
2024-12-07 11:09:55 +0100

    chore(nvim): fix dashboard header

--- nvim/dot-config/nvim/lua/plugins/snacks-nvim.lua
+++ nvim/dot-config/nvim/lua/plugins/snacks-nvim.lua
@@ -1,16 +1,19 @@
 return {
   {
-    "nvimdev/dashboard-nvim",
-    opts = function(_, opts)
-      local logo = [[ 
+    "folke/snacks.nvim",
+    opts = {
+      dashboard = {
+        preset = {
+          header = [[ 
 I use Neovim by the way ...
 I use Colemak-DH by the way ...
 I use a tiling window manager by the way ...
 I DO NOT use Arch by the way ...
 
 (((    THIS IS THE WAY!    )))
-      ]]
-      opts.config.header = vim.split(logo, "\n")
-    end,
+      ]],
+        },
+      },
+    },
   },
 }
