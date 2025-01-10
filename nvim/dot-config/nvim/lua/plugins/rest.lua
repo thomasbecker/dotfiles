5c5f212028f18f34c49ec62f40bfd775bcf11ac1
Thomas Becker <thomas.becker00@gmail.com>
2025-01-10 09:18:45 +0100

    chore(rest.nvim): update plugin setup to use lazy.nvim for luarocks dependencies

--- nvim/dot-config/nvim/lua/plugins/rest.lua
+++ nvim/dot-config/nvim/lua/plugins/rest.lua
@@ -1,18 +1,15 @@
 return {
   {
-    "vhyrro/luarocks.nvim",
-    priority = 1000,
-    config = true,
-    opts = {
-      rocks = { "lua-curl", "nvim-nio", "mimetypes", "xml2lua" },
-    },
-  },
-  {
     "rest-nvim/rest.nvim",
+    dependencies = {
+      "nvim-treesitter/nvim-treesitter",
+      opts = function(_, opts)
+        opts.ensure_installed = opts.ensure_installed or {}
+        table.insert(opts.ensure_installed, "http")
+      end,
+    },
     ft = "http",
-    dependencies = { "luarocks.nvim" },
     config = function()
-      require("rest-nvim").setup()
       require("telescope").load_extension("rest")
     end,
     keys = {
