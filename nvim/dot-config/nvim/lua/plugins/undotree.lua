e6b06e9f4027f905268cdf33a9a1b5fa829cb478
Thomas Becker <thomas.becker00@gmail.com>
2025-01-23 12:09:29 +0100

    feat(nvim): add undotree plugin

---
+++ nvim/dot-config/nvim/lua/plugins/undotree.lua
@@ -0,0 +1,14 @@
+return {
+  {
+    "mbbill/undotree",
+    lazy = true,
+    cmd = "UndotreeToggle",
+    keys = {
+      {
+        "<leader>uu",
+        "<cmd>UndotreeToggle<cr>",
+        desc = "Undotree toggle",
+      },
+    },
+  },
+}
