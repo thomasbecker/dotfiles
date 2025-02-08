a68a924a022a3d27ea61cf3243d9f05c9c612e5c
Thomas Becker <thomas.becker00@gmail.com>
2025-02-08 08:53:34 +0100

    chore(nvim): readd aider keymaps or quick file add/drop

--- nvim/dot-config/nvim/lua/plugins/nvim-aider.lua
+++ nvim/dot-config/nvim/lua/plugins/nvim-aider.lua
@@ -10,8 +10,8 @@ return {
       { "<leader>ais", "<cmd>AiderTerminalSend<cr>", desc = "Send to Aider", mode = { "n", "v" } },
       { "<leader>aic", "<cmd>AiderQuickSendCommand<cr>", desc = "Send Command To Aider" },
       { "<leader>aib", "<cmd>AiderQuickSendBuffer<cr>", desc = "Send Buffer To Aider" },
-      -- { "<leader>a+", "<cmd>AiderQuickAddFile<cr>", desc = "Add File to Aider" },
-      -- { "<leader>a-", "<cmd>AiderQuickDropFile<cr>", desc = "Drop File from Aider" },
+      { "<leader>a+", "<cmd>AiderQuickAddFile<cr>", desc = "Add File to Aider" },
+      { "<leader>a-", "<cmd>AiderQuickDropFile<cr>", desc = "Drop File from Aider" },
       { "<leader>air", "<cmd>AiderQuickReadOnlyFile<cr>", desc = "Add File as Read-Only" },
       -- Example nvim-tree.lua integration if needed
       { "<leader>a+", "<cmd>AiderTreeAddFile<cr>", desc = "Add File from Tree to Aider", ft = "NvimTree" },
